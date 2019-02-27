4d38941af42953d068be5edd5eeec61560816f94
Tony Li <litony1999@gmail.com>
2019-02-26 23:20:10 -0500

    exercise 1 donzo

---
+++ exercise1.py
@@ -0,0 +1,25 @@
+def draw_shape(options):
+  shape = ""
+
+  for r in range(options['rows']):
+    for c in range(options['cols']):
+      shape += options['char']
+
+    shape += "\n"
+
+
+  return shape
+
+
+
+print(draw_shape({
+    'rows': 4,
+    'cols': 4,
+    'char': '*'
+}))
+
+print(draw_shape({
+    'rows': 3,
+    'cols': 9,
+    'char': '0'
+}))
